5c68f58ddb2ebfa1dcb23b3f2fa705076c8932f8
Max Kalhama <kalhama.max@gmail.com>
2021-01-15 19:31:58 +0200

    Create create_database.sh

---
+++ create_database.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# create database
+read -p "Give database name: " dbname
+[ -z $dbname ] && echo "dbname cannot be empty" && exit 1
+CREATE="create database \"$dbname\";"
+
+# disallow everyone accessing it. This should be unnecessary if 
+# REVOKE CONNECT ON DATABASE template1 FROM PUBLIC;
+# has been ever run
+# see: https://dba.stackexchange.com/questions/17790/created-user-can-access-all-databases-in-postgresql-without-any-grants
+PERMISSIONS="REVOKE connect ON DATABASE \"$dbname\" FROM PUBLIC;"
+
+# create new user
+read -p "Give user name [$dbname]: " username
+username=${username:-$dbname}
+
+read -p "Give user passwork [$dbname]: " password
+password=${password:-$dbname}
+
+USER="create user \"$username\" with encrypted password '$password';"
+
+# grant privileges on database to new user
+GRANT="grant all privileges on database \"$username\" to \"$username\";"
+
+echo "Next run"
+echo "psql -c \"$CREATE $PERMISSIONS $USER $GRANT\""
